8c3647c233c097492cbf14557a804a73a571e8d1
Feng Haoran <harrifeng@gmail.com>
2017-10-14 02:00:22 +0000

    add one test case

--- all_test.go
+++ all_test.go
@@ -69,3 +69,26 @@ func Test_lengthOfLongestSubstring(t *testing.T) {
 		})
 	}
 }
+
+// 4.median-of-two-sorted-arrays.go
+func Test_findMedianSortedArrays(t *testing.T) {
+	type args struct {
+		nums1 []int
+		nums2 []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		{"", args{[]int{1, 2}, []int{3, 4, 5}}, 3},
+		{"", args{[]int{}, []int{1, 2}}, 1.5},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMedianSortedArrays(tt.args.nums1, tt.args.nums2); got != tt.want {
+				t.Errorf("findMedianSortedArrays() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
